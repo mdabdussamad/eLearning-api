d51d580a33051ee50604616a7731809f52a61677
Md. Abdussamad <30697752+mdabdussamad@users.noreply.github.com>
2025-10-15 03:09:01 +0545

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
-require("dotenv").config();
+import dotenv from 'dotenv';
+dotenv.config();
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
